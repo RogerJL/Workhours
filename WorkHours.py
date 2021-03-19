0e318585bd05ec2f7aab1afafe5b276c724d13ce
RogerJL <roger.larsson@e-gatan.se>
2021-03-19 23:16:02 +0100

    Updated for Windows
    Suspended sleep
    Fixed static code review errors

--- WorkHours.py
+++ WorkHours.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-from datetime import datetime,timedelta
+from datetime import datetime
 import errno
 import dateutil.parser
 
 from pynput.mouse import Button, Controller
 from time import sleep
-from math import sin
 import traceback
 import os
 import sys
@@ -26,13 +25,14 @@ ADDED_UP_UNTIL = DATA_CACHE + 'LastDaily.txt'  # Last Daily addup
 CRASH_LOG = DATA_ROOT + 'CrashLog.txt'
 
 
-def AppendLast(now):
+def append_last(now):
     f = open(LAST_TIMESTAMPS, 'a+')
     f.write(now.isoformat() + '\n')
     f.close()
     return now
 
-def GetLoggedActivity():
+
+def get_logged_activity():
     try:
         with open(LAST_TIMESTAMPS, 'r') as f:
             timestamps = f.readlines()
@@ -44,7 +44,8 @@ def GetLoggedActivity():
         return None, None
     return dateutil.parser.parse(timestamps[-1]), dateutil.parser.parse(timestamps[0])
 
-def EndSection(started, ended):
+
+def end_section(started, ended):
     if not started or not ended:
         print(f"Section can't be ended started={started} ended={ended}")
         return
@@ -53,17 +54,17 @@ def EndSection(started, ended):
     message = f'{started.isoformat(" ", "minutes")} till {ended.strftime("%H:%M")} = {hours} timmar'
     print(message)
     
-    Addtext(started, message)
+    add_text(started, message)
 
     # added worktime section to output, restart section
-    os.rename(LAST_TIMESTAMPS, LAST_TIMESTAMPS + ".old")
+    os.replace(LAST_TIMESTAMPS, LAST_TIMESTAMPS + ".old")
     
 
-def Addtext(started, text):
+def add_text(started, text):
     current_week = datetime.isocalendar(started)[1]
 
     week_dir = f'v{current_week}'
-    if not week_dir in os.listdir(DATA_ROOT):
+    if week_dir not in os.listdir(DATA_ROOT):
         os.mkdir(DATA_ROOT + week_dir)
 
     p = f"{DATA_ROOT}{week_dir}/{started.strftime('%Y-%m-%d')}.txt"
@@ -80,6 +81,7 @@ def mkdir(dirname):
         if exc.errno != errno.EEXIST:
             raise
 
+
 # Main program
 try:
     mkdir(DATA_ROOT)
@@ -87,14 +89,14 @@ try:
 
     activity = None  # Unknown
     opos = None
-    logged_activity, oldest_activity = GetLoggedActivity()
+    logged_activity, oldest_activity = get_logged_activity()
     print("First", oldest_activity, "Last", logged_activity)
     if logged_activity:
-        EndSection(oldest_activity, logged_activity) 
+        end_section(oldest_activity, logged_activity)
         if (datetime.now() - logged_activity).total_seconds() < TIMEOUT_SECONDS:
             print("restarted within timeout, can't tell if by system or user")
             oldest_activity = logged_activity
-            logged_activity = AppendLast(oldest_activity)
+            logged_activity = append_last(oldest_activity)
         else:
             print("restarted after timeout passed (or section already ended)")
             oldest_activity = None
@@ -112,14 +114,31 @@ try:
             oldest_activity = activity
         sys.stdout.write('W')
         sys.stdout.flush()
-        logged_activity = AppendLast(activity)
+        logged_activity = append_last(activity)
 
         stopped = TIMEOUTS
         opos = mouse.position
+        prev_looptime = datetime.now()
         while stopped > 0:
             sleep(SHORT_SLEEP)
-            npos = mouse.position
-            stopped -= 1
+            next_looptime = datetime.now()
+            slept = (next_looptime - prev_looptime).total_seconds()
+            if slept > 3*SHORT_SLEEP:
+                sys.stdout.write('z')
+            if slept >= stopped:
+                sys.stdout.write('Z')
+                npos = opos
+                stopped = 0
+                # No longer active, will write
+                sys.stdout.write('s\n')
+                end_section(oldest_activity, activity)
+                activity = None
+                oldest_activity = None
+                logged_activity = None
+            else:
+                npos = mouse.position
+                stopped -= slept
+            prev_looptime = next_looptime
             if opos != npos:
                 # Continued activity, no writes
                 sys.stdout.write('.')
@@ -131,28 +150,20 @@ try:
                 if seconds_since_last_log > 2 * TIMEOUT_SECONDS:
                     # must have slept, end old section
                     sys.stdout.write('S+W\n')
-                    EndSection(oldest_activity, logged_activity)
+                    end_section(oldest_activity, logged_activity)
                     # activity OK
                     oldest_activity = activity  # not yet written
-                    logged_activity = AppendLast(activity)
+                    logged_activity = append_last(activity)
                 elif seconds_since_last_log >= TIMEOUT_SECONDS:
                     sys.stdout.write('w')
                     sys.stdout.flush()
-                    logged_activity = AppendLast(activity)
-            elif stopped == 0:
-                # No longer active, will write
-                sys.stdout.write('s\n')
-                EndSection(oldest_activity, activity)
-                activity = None
-                oldest_activity = None
-                logged_activity = None
+                    logged_activity = append_last(activity)
 
 except Exception:
-    # os.rename(CRASH_LOG, CRASH_LOG + ".old")
+    # os.replace(CRASH_LOG, CRASH_LOG + ".old")
     log = open(CRASH_LOG, 'w')
     
     traceback.print_exc(file=log)
     
     log.close()
     print("Crashlog written\n")
-
